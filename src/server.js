5885bcfda9c91a355d6f30544e41c293efaeb4e0
Lucas Andrade <contato.developerlucas@gmail.com>
2023-08-23 18:12:04 -0300

    using Insomnia to test POST method. Separating routes in specific folder and file

--- src/server.js
+++ src/server.js
@@ -1,21 +1,12 @@
 const express = require("express");
+const routes = require("./routes")
 
 const app = express();
 
-app.get("/message/:id/:user", (request, response) => {   // "ROTA ESCOLHIDA", request, response. Definir a resposta com o send
-    
-    const { id, user } = request.params // Desestruturando o request params para receber o id e o user, evita repetir o codigo
-    
-    // response.send(`Mensagem de ID: ${request.params.id}. Para o usuário: ${request.params.user}`) Metodo antigo
+app.use(express.json())
+app.use(routes)
 
-    response.send(`Mensagem ID: ${id}. Para o usuário: ${user}`) // Novo método
-})
 
-app.get("/user", (request, response) => {
-    const { page, limit } = request.query // Desestruturando as informações que eu quero do metodo query.
-
-    response.send(`Página: ${page}. Limitado a ${limit}.`)
-})
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));

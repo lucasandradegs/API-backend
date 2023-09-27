78285214a7a2526d1f202d60d96f65bd36fe48ea
Lucas Andrade <lucas.goncalvessousa@aperam.com>
2023-09-27 10:17:46 -0300

    configuring pm2 and .env

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors")
+require("dotenv/config")
 
 const express = require("express")
 const routes = require("./routes")
@@ -33,5 +34,5 @@ app.use((error, request, response, next) => {
 })
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
